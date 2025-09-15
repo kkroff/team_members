771ddc601f0bafa5f49343e6bc5ea14a15a59588
Christoph Jerchel <c.jerchel@gmail.com>
2025-09-16 00:07:37 +0200

    SCSS Struktur, Navigation, Hero-Image, Content Styles

--- config/system/settings.php
+++ config/system/settings.php
@@ -39,11 +39,6 @@ return [
             'automaticInstallation' => '1',
             'offlineMode' => '0',
         ],
-        'vite_asset_collector' => [
-            'defaultManifest' => '_assets/vite/.vite/manifest.json',
-            'devServerUri' => 'auto',
-            'useDevServer' => 'auto',
-        ],
     ],
     'FE' => [
         'cacheHash' => [
